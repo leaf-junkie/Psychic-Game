02f3b5a4a206d8aece0ac9747d1fb4804895513f
leaf-junkie <ragsdale.jar@gmail.com>
2018-12-29 16:49:17 -0500

    all aspects of the game are properly working!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,25 +2,18 @@
 document.onkeyup = keyUpHandler;
 
 // Generate random magic letter
-var letters = "abcdefghijklmnopqrstuvwxyz";
+let letters = 'abcdefghijklmnopqrstuvwxyz';
 letters = letters.toUpperCase();
-var randomIndex = Math.floor(Math.random() * letters.length);
-var magicLetter = letters[randomIndex];
+let randomIndex = Math.floor(Math.random() * letters.length);
+let magicLetter = letters[randomIndex];
 
-var guessedLetters = [];
+let guessedLetters = [];
 
 console.log(randomIndex);
 console.log(magicLetter);
 
-var guessesRemaining = 10;
-document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
-
-var buttonYes = document.getElementsByClassName("button-yes").onclick = click();
-var buttonNo = document.getElementsByClassName("button-no").onclick = click();
-
-// var winScreen = document.getElementById("winScreen") = ;
-
-// var loseScreen = document.getElementById("loseScreen") = ;
+let guessesRemaining = 10;
+document.getElementById('guessesRemaining').innerHTML = guessesRemaining;
 
 // This is the logic for the game
 // Get input from user when a key is pressed
@@ -39,8 +32,8 @@ function keyUpHandler(event) {
         // If it has been pressed, return false
         guessedLetters.push(event.key);
 
-        // Display number of guesses remaining under "Your Guesses Thus Far"
-        document.getElementById("guessedLetters").innerHTML = guessedLetters.join(", ");
+        // Display number of guesses remaining under 'Your Guesses Thus Far'
+        document.getElementById('guessedLetters').innerHTML = guessedLetters.join(', ');
     }
 
     // Show pressed keys in console
@@ -50,60 +43,60 @@ function keyUpHandler(event) {
     if (event.key.toUpperCase() === magicLetter.toUpperCase()) {
         // If it is the magic letter, win (call a win function)
         win();
-        reset();
     } else {
         // If not, decrement guess count
         guessesRemaining -= 1;
     }
 
-    // Display the number of remaining guesses under "Guesses Remaining"
-    console.log("Number of guesses remaining: " + guessesRemaining);
-    document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
+    // Display the number of remaining guesses under 'Guesses Remaining'
+    console.log('Number of guesses remaining: ' + guessesRemaining);
+    document.getElementById('guessesRemaining').innerHTML = guessesRemaining;
 
     // If zero guesses left, lose (call a lose function) and reset guesses remaining
     if (guessesRemaining === 0) {
         lose();
-        reset();
     }
 }
 
 function win() {
-    console.log("You win!");
+    console.log('You win!');
     // Display number of wins in browser
-    var wins = parseInt(document.getElementById("winCount").innerHTML);
+    let wins = parseInt(document.getElementById('winCount').innerHTML);
     wins += 1;
-    document.getElementById("winCount").innerHTML = wins;
+    document.getElementById('winCount').innerHTML = wins;
 
     // Display win screen
-    
+    let winScreen = document.getElementById('winScreen');
+    winScreen.style.visibility = 'visible';
 }
 
 function lose() {
-    console.log("You lose!");
+    console.log('You lose!');
     // Display number of losses in browser
-    var losses = parseInt(document.getElementById("lossCount").innerHTML);
+    let losses = parseInt(document.getElementById('lossCount').innerHTML);
     losses += 1;
-    document.getElementById("lossCount").innerHTML = losses;
+    document.getElementById('lossCount').innerHTML = losses;
     
     // Display lose screen
-
+    let loseScreen = document.getElementById('loseScreen');
+    loseScreen.style.visibility = 'visible';
 }
 
 function reset() {
     // Reset guesses remaining
     guessesRemaining = 10;
-    document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
+    document.getElementById('guessesRemaining').innerHTML = guessesRemaining;
 
     // Empty guessed letters array
     guessedLetters = [];
-    document.getElementById("guessedLetters").innerHTML = guessedLetters.join(", ");
+    document.getElementById('guessedLetters').innerHTML = guessedLetters.join(', ');
 
     // Select new magic letter
     randomIndex = Math.floor(Math.random() * letters.length);
     magicLetter = letters[randomIndex];
     console.log(magicLetter);
+
+    // Make win screen and lose screen hidden on reset
+    winScreen.style.visibility = 'hidden';
+    loseScreen.style.visibility = 'hidden';
 }
-
-// function click() {
-
-// }
