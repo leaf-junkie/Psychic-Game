b4cb769ba61e91ca1f213313907cb05933d7cfae
leaf-junkie <ragsdale.jar@gmail.com>
2018-12-29 00:07:07 -0500

    stylistic changes made to page text and divs; endgame screens created but not finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,38 +13,19 @@ console.log(randomIndex);
 console.log(magicLetter);
 
 var guessesRemaining = 10;
-
-// var nonLetters = event.keys[];
+document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
 
 // This is the logic for the game
 // Get input from user when a key is pressed
 function keyUpHandler(event) {
-    // Show pressed keys in console
-    console.log(guessedLetters);
-
-    // Check if key is the magic letter
-    if (event.key.toUpperCase() === magicLetter.toUpperCase()) {
-        // If it is the magic letter, win (call a win function)
-        win(); 
-        reset();
-    } else {
-        // If not, decrement guess count
-        guessesRemaining -= 1;
+    // Filter non letters
+    if (!letters.includes(event.key.toUpperCase())) {
+        return;
     }
-
-    // If zero guesses left, lose (call a lose function) and reset guesses remaining
-    if (guessesRemaining === 0) {        
-        lose();
-        reset();
-    }
-    
-    // Display the number of remaining guesses under "Guesses Remaining"
-    console.log("Number of guesses remaining: " + guessesRemaining);
-    document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
 
     // Ignore redundant input from user
     // On key press, check if the letter has already been pressed
-    if (guessedLetters.includes(event.key)) {    
+    if (guessedLetters.includes(event.key)) {
         // If it has not been pressed, return true
         return;
     } else {
@@ -52,10 +33,32 @@ function keyUpHandler(event) {
         guessedLetters.push(event.key);
 
         // Display number of guesses remaining under "Your Guesses Thus Far"
-        guessedLetters.forEach(function(letter) {
-        console.log(letter);
-        document.getElementById("guessedLetters").innerHTML = guessedLetters + " ";
-        });
+        document.getElementById("guessedLetters").innerHTML = guessedLetters.join(", ");
+    }
+
+    // Show pressed keys in console
+    console.log(guessedLetters);
+
+    // Check if key is the magic letter
+    if (event.key.toUpperCase() === magicLetter.toUpperCase()) {
+        // If it is the magic letter, win (call a win function)
+        win();
+        reset();
+    } else {
+        // If not, decrement guess count
+        guessesRemaining -= 1;
+    }
+
+    // Display the number of remaining guesses under "Guesses Remaining"
+    console.log("Number of guesses remaining: " + guessesRemaining);
+    document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
+
+    // If zero guesses left, lose (call a lose function) and reset guesses remaining
+    if (guessesRemaining === 0) {
+        lose();
+        reset();
+    }
+}
 
 function win() {
     console.log("You win!");
@@ -63,7 +66,7 @@ function win() {
     var wins = parseInt(document.getElementById("winCount").innerHTML);
     wins += 1;
     document.getElementById("winCount").innerHTML = wins;
-    alert("As I suspected...You knew which letter I had in mind. Would you like to play again?");
+    // alert("As I suspected...You knew which letter I had in mind. Would you like to play again?");
 }
 
 function lose() {
@@ -72,20 +75,17 @@ function lose() {
     var losses = parseInt(document.getElementById("lossCount").innerHTML);
     losses += 1;
     document.getElementById("lossCount").innerHTML = losses;
-    alert("Hmm...Perhaps I was mistaken. You were unable to determine which letter I was thinking of. Would you like to try again?");
+    // alert("Hmm...Perhaps I was mistaken. You were unable to determine which letter I was thinking of. Would you like to try again?");
 }
 
 function reset() {
     // Reset guesses remaining
     guessesRemaining = 10;
-    document.getElementById("guessesRemaining").counterReset = guessesRemaining;
-
-    // Reset remaining guesses 
-    guessesRemaining = 10;
     document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
 
     // Empty guessed letters array
     guessedLetters = [];
+    document.getElementById("guessedLetters").innerHTML = guessedLetters.join(", ");
 
     // Select new magic letter
     randomIndex = Math.floor(Math.random() * letters.length);
